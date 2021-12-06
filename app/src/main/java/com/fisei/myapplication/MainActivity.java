45a2c21755cf6b6b491b5bc63ed040f9bdfd5468
Vanessa Llerena <vanestefania01@gmail.com>
2021-12-05 20:29:53 -0500

    git commit - m "20:29"

--- app/src/main/java/com/fisei/myapplication/MainActivity.java
+++ app/src/main/java/com/fisei/myapplication/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private EditText n1;
-
+    int TextView = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
